b479246f24e56dfb6e16cdcfc20a3dbb0012d223
CYoung5233 <cyoung17@uw.edu>
2024-11-07 15:25:12 -0800

    Made book routes require valid JWT

--- src/routes/book-routes/index.ts
+++ src/routes/book-routes/index.ts
@@ -1,16 +1,25 @@
 import express, { Router } from 'express';
+import { checkToken } from '../../core/middleware';
+import { tokenTestRouter } from '../closed/tokenTest';
+
 import getBooksByAuthor from './GET/getBooksByAuthor';
 import getBooksByISBN from './GET/getBooksByISBN';
 import getBooksByRating from './GET/getBooksByRating';
 import getBooksByTitle from './GET/getBooksByTitle';
 import getBooksByPublicationYear from './GET/getBooksByPublicationYear';
+import postNewBook from './POST/postNewBook';
 
 const booksRouter: Router = express.Router();
 
-booksRouter.use(getBooksByAuthor);
-booksRouter.use(getBooksByISBN);
-booksRouter.use(getBooksByRating);
-booksRouter.use(getBooksByTitle);
-booksRouter.use(getBooksByPublicationYear);
+booksRouter.use(
+    checkToken,
+    tokenTestRouter,
+    getBooksByAuthor,
+    getBooksByISBN,
+    getBooksByRating,
+    getBooksByTitle,
+    getBooksByPublicationYear,
+    postNewBook
+);
 
 export { booksRouter };

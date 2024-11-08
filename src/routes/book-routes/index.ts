d8a0441e3e97d34398b633c64b61e5e11c3968a3
CYoung5233 <cyoung17@uw.edu>
2024-11-08 15:43:03 -0800

    Indexed the new routes

--- src/routes/book-routes/index.ts
+++ src/routes/book-routes/index.ts
@@ -2,11 +2,15 @@ import express, { Router } from 'express';
 import { checkToken } from '../../core/middleware';
 import { tokenTestRouter } from '../closed/tokenTest';
 
+import getAllBooks from './GET/getAllBooks';
 import getBooksByAuthor from './GET/getBooksByAuthor';
 import getBooksByISBN from './GET/getBooksByISBN';
 import getBooksByRating from './GET/getBooksByRating';
 import getBooksByTitle from './GET/getBooksByTitle';
 import getBooksByPublicationYear from './GET/getBooksByPublicationYear';
+import patchRate from './PATCH/patchRate';
+import patchUpdateBook from './PATCH/patchUpdateBook';
+import patchUpdateRatings from './PATCH/patchUpdateRating';
 import postNewBook from './POST/postNewBook';
 
 const booksRouter: Router = express.Router();
@@ -14,11 +18,15 @@ const booksRouter: Router = express.Router();
 booksRouter.use(
     checkToken,
     tokenTestRouter,
+    getAllBooks,
     getBooksByAuthor,
     getBooksByISBN,
     getBooksByRating,
     getBooksByTitle,
     getBooksByPublicationYear,
+    patchRate,
+    patchUpdateBook,
+    patchUpdateRatings,
     postNewBook
 );
 

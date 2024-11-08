d7080514bb547fb097826e949f56bd7723817ba6
asaran07 <saran.arsh07@icloud.com>
2024-11-08 14:22:51 -0800

    Add delete routes in router imports

--- src/routes/book-routes/index.ts
+++ src/routes/book-routes/index.ts
@@ -8,6 +8,8 @@ import getBooksByRating from './GET/getBooksByRating';
 import getBooksByTitle from './GET/getBooksByTitle';
 import getBooksByPublicationYear from './GET/getBooksByPublicationYear';
 import postNewBook from './POST/postNewBook';
+import deleteBookByISB from './DELETE/deleteBookByISBN'
+import deleteBookRange from './DELETE/deleteBookRange'
 
 const booksRouter: Router = express.Router();
 
@@ -19,7 +21,9 @@ booksRouter.use(
     getBooksByRating,
     getBooksByTitle,
     getBooksByPublicationYear,
-    postNewBook
+    postNewBook,
+    deleteBookByISB,
+    deleteBookRange
 );
 
 export { booksRouter };
